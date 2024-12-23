86f9c875592583c2640ae50608e2c2f49ee01488
jyclave <jyclave@gmail.com>
2024-12-23 15:34:00 +0100

    fetch categories

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -3,6 +3,47 @@ function displayWorks() {
   let divGallery = document.querySelector(".gallery");
   // suite du code à faire ici pour l instant
   console.log(fetch("http://localhost:5678/api/works"));
+	//faire un fetch et boucler sur const work//
 }
 
 displayWorks();
+
+const filtersElement = document.querySelector(".filters");// emplacement pour ajouter les buttons//
+
+function displayCategories() {
+
+	fetch("http://localhost:5678/api/categories")
+	.then ((response) => {
+	return response.json()})
+	.then ((categories) => {
+		for (const category of categories) //a chaque tour de boucle sur categories on stocke les informations dans une variable category//
+		{
+			const filterElement = document.createElement("button");
+		filterElement.innerText = category.name;
+
+		if (filtersElement) {
+			filtersElement.appendChild(filterElement);
+		console.log(category)		
+		}
+		}
+		
+	});
+}
+displayCategories();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
